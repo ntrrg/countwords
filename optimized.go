b16a100b31e48aa0ca637d4059fa91ee444a1771
Miguel Angel <ntrrgx@gmail.com>
2021-03-17 23:14:16 -0400

    Remove WordCounter abstraction

--- optimized.go
+++ optimized.go
@@ -8,77 +8,76 @@ import (
 )
 
 func main() {
-	wc := NewWordCounter()
-	io.Copy(wc, os.Stdin)
+	var word []byte
+	buf := make([]byte, 64*1024)
+	counts := make(map[string]*int)
 
-	for _, word := range wc.MostCommon() {
-		fmt.Println(word.Word, word.Count)
-	}
-}
+	for {
+		// Read input in 64KB blocks till EOF.
+		n, err := os.Stdin.Read(buf)
+		if err != nil && err != io.EOF {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 
-type WordCount struct {
-	Word  string
-	Count int
-}
+		if n == 0 {
+			break
+		}
 
-type WordCounter struct {
-	buf   []byte
-	words map[string]*int
-}
+		// Count words in the buffer.
+		for i := 0; i < n; i++ {
+			c := buf[i]
+			buf[i] = 0 // Reset the buffer as we go.
 
-func NewWordCounter() *WordCounter {
-	return &WordCounter{
-		words: make(map[string]*int),
-	}
-}
+			if c == ' ' || c == '\n' {
+				if len(word) > 0 {
+					increment(counts, word)
+					word = word[:0]
+				}
 
-func (wc *WordCounter) addWord() {
-	if len(wc.buf) == 0 {
-		return
-	}
+				continue
+			}
 
-	// Clean buffer after counting a word
-	defer func() { wc.buf = wc.buf[:0] }()
+			// Convert to ASCII lowercase in place as we go.
+			if c >= 'A' && c <= 'Z' {
+				c = c + ('a' - 'A')
+			}
 
-	if n, ok := wc.words[string(wc.buf)]; ok {
-		*n++
-		return
-	}
-
-	n := 1
-	wc.words[string(wc.buf)] = &n
-}
-
-func (wc *WordCounter) MostCommon() []WordCount {
-	// Flush any data in the buffer
-	wc.addWord()
-
-	counts := make([]WordCount, 0, len(wc.words))
-	for word, count := range wc.words {
-		counts = append(counts, WordCount{word, *count})
+			word = append(word, c)
+		}
 	}
 
-	sort.Slice(counts, func(i, j int) bool {
-		return counts[i].Count > counts[j].Count
+	// Count last word, if any.
+	if len(word) > 0 {
+		increment(counts, word)
+	}
+
+	ordered := make([]Count, 0, len(counts))
+	for word, count := range counts {
+		ordered = append(ordered, Count{word, *count})
+	}
+
+	sort.Slice(ordered, func(i, j int) bool {
+		return ordered[i].Count > ordered[j].Count
 	})
 
-	return counts
+	for _, count := range ordered {
+		fmt.Println(count.Word, count.Count)
+	}
 }
 
-func (wc *WordCounter) Write(p []byte) (int, error) {
-	for _, b := range p {
-		if b == ' ' || b == '\n' {
-			wc.addWord()
-			continue
-		}
-
-		// To lower case
-		if b >= 'A' && b <= 'Z' {
-			b += 'a' - 'A'
-		}
-
-		wc.buf = append(wc.buf, b)
+func increment(counts map[string]*int, word []byte) {
+	if p, ok := counts[string(word)]; ok {
+		// Word already in map, increment existing int via pointer.
+		*p++
+		return
 	}
+	// Word not in map, insert new int.
+	n := 1
+	counts[string(word)] = &n
+}
 
-	return len(p), nil
+type Count struct {
+	Word  string
+	Count int
 }
